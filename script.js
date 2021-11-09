646f6331caff4dcd3d64bcd42b81121c07afd487
Jefferson [Jeffrey] <dantas.jefferson03@gmail.com>
2021-11-09 16:15:51 -0300

    requisito 3 ok

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const btnSubmit = document.getElementById('btn-submit');
 btnSubmit.addEventListener('click', (event) => {
   event.preventDefault();
-  const email = document.getElementsByName('email');
-  const senha = document.getElementsByName('password');
-  console.log(email, senha);
+  const email = document.getElementsByName('email')[0].value;
+  const senha = document.getElementsByName('password')[0].value;
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 });
