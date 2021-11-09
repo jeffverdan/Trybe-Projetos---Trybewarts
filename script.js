fb35ff14e68e298658011d81537f6a36ef8bb17f
Eric Schineider <eric-schineider@outlook.com>
2021-11-09 16:19:20 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -6,39 +6,33 @@ const checkBox = document.getElementById('agreement');
 let counter = document.getElementById('counter');
 const textarea = document.getElementById('textarea');
 // Mensagem e efeito de login
-btnEnter.addEventListener('click', function(){
-if(email.value == "tryber@teste.com" && password.value == "123456") {
-    alert('Olá, Tryber!')
-} else {
-alert("Email ou senha inválidos.")
-}
+btnEnter.addEventListener('click', function () {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  };
 });
 
 // Habilitar botão de enviar
 window.onload = () => {
-    btnSend.disabled = true
-}
-    
+  btnSend.disabled = true;
+};
 
-checkBox.addEventListener('click', function(){
-       
-    if(checkBox.checked) {
-        btnSend.disabled = false;
-    } else {
-        btnSend.disabled = true;
-    }
-})
+checkBox.addEventListener('click', function () {
+  if (checkBox.checked) {
+    btnSend.disabled = false;
+  } else {
+    btnSend.disabled = true;
+  };
+});
 
-
-// Contagem de caracteres 
+// Contagem de caracteres
 
 let textarea2 = textarea.getAttribute('maxlength');
 counter.innerHTML = textarea2;
 textarea.addEventListener('keyup', function () {
-   document.getElementById('counter').innerHTML = textarea2 - this.value.length;
+  document.getElementById('counter').innerHTML = textarea2 - this.value.length;
 });
 
 counter.innerHTML = textarea2;
-
-
-
