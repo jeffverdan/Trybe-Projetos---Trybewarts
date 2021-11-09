8365bf97293a4fe2c66aabab2b6fa54d0d4f546b
Iago Medeiros <iagomedeiros.dev@gmail.com>
2021-11-09 17:11:09 -0300

    Requisito 13 ok

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ const selecionaButao = document.querySelector(".header-button");
 selecionaButao.addEventListener("click", alerta);
 
 function alerta() {
-  const emailInput = document.querySelector("#input-email");
-  const senhaInput = document.querySelector("#input-senha");
+  const emailInput = document.querySelector("#email-header");
+  const senhaInput = document.querySelector("#senha-header");
 
-  if (
-    emailInput.value === "tryber@teste.com" &&
-    senhaInput.value === "123456"
-  ) {
+  if (emailInput.value === "tryber@teste.com" && senhaInput.value === "123456"){
     alert("Olá, Tryber!");
   } else {
-    alert("Email ou senha inválido");
+    alert("Email ou senha inválidos.");
   }
 }
