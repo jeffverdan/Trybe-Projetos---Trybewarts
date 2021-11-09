a9ef0346d037958da7d10e0913245ef2a73c4187
Igor <igormelojunqueira@gmail.com>
2021-11-09 15:42:43 -0300

    Lint Arrumado ate requisito 11

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Requisito 3
 function login() {
   const email = document.getElementById('email-input');
   const password = document.getElementById('password-input');
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     return alert('Olá, Tryber!');
-} 
+  }
   return alert('Email ou senha inválidos.');
 }
 
