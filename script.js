1933f4ef5a8a145632103324def3301e96e271e3
Antonio Sabino <aslimajr@outlook.com>
2021-11-09 16:12:02 -0300

    Resolucao requisito 4 e correcao requisito 3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const senha = document.getElementById('senha');
 const btnLogin = document.getElementById('btn-login');
 
 btnLogin.addEventListener('click', () => {
-  if (email.value === 'tryber@teste.com' && senha.value === 123456) {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
