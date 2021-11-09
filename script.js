3ea4d4a69f90faae4accbdf9c256c578523e3b73
MatheusMena <matheusmena1@hotmail.com>
2021-11-09 17:20:30 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('btn');
+
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+
+button.addEventListener('click', () => {
+  if (input1.value === 'tryber@teste.com' && input2.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
