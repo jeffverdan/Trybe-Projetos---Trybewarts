ac9fd7cddba69bad5edc63894f4675927d6fc474
AdrianFadiga <adrianmfadiga@gmail.com>
2021-11-09 18:47:01 -0300

    finalizado 80%

--- script.js
+++ script.js
@@ -14,3 +14,41 @@ function alertEmailAndPassword() {
 }
 
 btnHeader.addEventListener('click', alertEmailAndPassword);
+// Criando os radio buttons (req 14)
+function createRadioButtons() {
+  const elementLabel = document.createElement('label');
+  elementLabel.id = 'label-rate';
+  elementLabel.innerText = 'Como você avalia a Trybewarts?';
+  const evaluationForm = document.getElementById('evaluation-form');
+  evaluationForm.appendChild(elementLabel);
+  for (let i = 0; i <= 10; i += 1) {
+    const radioButton = document.createElement('input');
+    radioButton.type = 'radio';
+    radioButton.innerHTML = i;
+    radioButton.value = i;
+    radioButton.name = 'rate';
+    evaluationForm.appendChild(radioButton);
+  }
+}
+createRadioButtons();
+// Criando o textarea (req 15)
+function createTextArea() {
+  const elementLabel = document.createElement('label');
+  elementLabel.className = 'textarea';
+  elementLabel.innerText = 'Deixe seu comentário:';
+  document.getElementById('evaluation-form').appendChild(elementLabel);
+  const textArea = document.createElement('textarea');
+  textArea.id = 'textarea';
+  textArea.maxLength = 500;
+  document.getElementById('evaluation-form').appendChild(textArea);
+}
+createTextArea();
+// Req 17 (pulei o 16)
+function createSubmitBtn() {
+  const submitBtn = document.createElement('button');
+  submitBtn.id = 'submit-btn';
+  submitBtn.innerText = 'Enviar';
+  submitBtn.type = 'submit';
+  document.getElementById('evaluation-form').appendChild(submitBtn);
+}
+createSubmitBtn();
