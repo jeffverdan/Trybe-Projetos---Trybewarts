c3d59574b75666fe93d9eb2d80ad8495458bb10c
SamuelDAlencar <sarl.2001@hotmail.com>
2021-11-09 18:49:22 -0300

    conclusão requisitos 16, 17, 18, 19, 20

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const btnEntrar = document.getElementById('btn-entrar');
+const btnEnviar = document.getElementById('submit-btn');
+const agreementButton = document.getElementById('agreement');
+const contador = document.getElementById('counter');
+const textArea = document.getElementById('textarea');
 
 btnEntrar.addEventListener('click', (event) => {
   if (email.value !== 'tryber@teste.com' && senha.value !== '123456') {
@@ -9,4 +13,18 @@ btnEntrar.addEventListener('click', (event) => {
   } else {
     alert('Olá, Tryber!');
   }
+});
+
+agreementButton.addEventListener('click', () => {
+  if (btnEnviar.disabled === true) {
+    btnEnviar.disabled = false;
+  } else {
+    btnEnviar.disabled = true;
+  }
+});
+
+textArea.addEventListener('input', () => {
+  const countMax = 500;
+  const count = countMax - textArea.value.length;
+  contador.innerText = count;
 });
