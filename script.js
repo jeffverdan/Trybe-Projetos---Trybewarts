47747e620dde2d40a2769b8f35b84ba2a97b43ab
MarcsVinicius <marcos.vgalencar@gmail.com>
2021-11-09 17:50:02 -0300

    feito requisito ate o 20

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const loginHeader = document.getElementById('botao-entrar');
 const emailHeader = document.getElementById('email');
 const senhaHeader = document.getElementById('senha');
+const submitregister = document.getElementById('submit-btn');
+const agreementcheck = document.getElementById('agreement');
+const textAreaInput = document.getElementById('textarea');
+const counter = document.getElementById('counter');
+let checked;
 
 loginHeader.addEventListener('click', () => {
   if (emailHeader.value === 'tryber@teste.com' && senhaHeader.value === '123456') {
@@ -8,4 +13,18 @@ loginHeader.addEventListener('click', () => {
   } else {
     alert('Email ou senha inválidos.');
   }
+});
+
+agreementcheck.addEventListener('click', () => {
+  if (checked === undefined || checked === true) {
+    submitregister.disabled = false;
+    checked = false;
+  } else {
+    checked = true;
+    submitregister.disabled = true;
+  }
+});
+
+textAreaInput.addEventListener('input', () => {
+  counter.innerText = 500 - textAreaInput.value.length;
 });
