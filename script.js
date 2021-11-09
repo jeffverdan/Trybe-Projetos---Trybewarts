fb0fa8f6e982759af94c324869c1ea559e035af0
Pedro Candido <pedrohcn02@gmail.com>
2021-11-09 16:19:18 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("#btnSubmit");
+const inputEmail = document.querySelector("#inputEmail");
+const inputPassword = document.querySelector("#inputPassword");
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (
+    inputEmail.value === "tryber@teste.com" &&
+    inputPassword.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+});
