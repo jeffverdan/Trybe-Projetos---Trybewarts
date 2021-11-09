4b87bf09e08a07085c27969afdab1b905444c10a
Giovanna <giovannaelizz@hotmail.com>
2021-11-09 18:57:27 -0300

    requisito 1, 2, 3 e 4 OK

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const email = document.querySelector('.email');
-const senha = document.querySelector('.senha');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
 const botao = document.querySelector('.button');
 
-function loginCorreto() {
-if (email === "tryber@teste.com" && senha === 123456) {
-    alert('Olá, Trybe!')
+function loginCorreto(event) {
+    event.preventDefault()
+
+    if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    alert('Olá, Tryber!')
 
 } else {
     alert('Email ou senha inválidos.')
