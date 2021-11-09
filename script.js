8ed5d3307bdeda112159b864325d984f023a2465
Pedro Candido <pedrohcn02@gmail.com>
2021-11-09 18:15:55 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('#btnSubmit');
 const inputEmail = document.querySelector('#inputEmail');
 const inputPassword = document.querySelector('#inputPassword');
+const submitBtn = document.querySelector('#submit-btn');
+const checkboxAgree = document.querySelector('#agreement');
 
 btn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -10,5 +12,13 @@ btn.addEventListener('click', (event) => {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+checkboxAgree.addEventListener('click', () => {
+  if (checkboxAgree.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
   }
 });
