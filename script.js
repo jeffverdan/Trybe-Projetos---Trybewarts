de2a5056e4fe804c25fc3033a226b0b16c84d9cb
Alana Correia <alanacorreia.ac@gmail.com>
2021-11-09 15:41:27 -0300

    realiza os requisitos 1-4

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnLogin = document.getElementById('btn-login');
+const inputEmail = document.getElementById('email');
+const inputPassword = document.getElementById('senha');
+
+function validationLogin() {
+  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+btnLogin.addEventListener('click', validationLogin);
