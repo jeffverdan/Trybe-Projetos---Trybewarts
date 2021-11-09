356cc57065e49a0cc76d73effe0d0310487caf1d
Rafael <rafalyf11@gmail.com>
2021-11-08 21:47:08 -0300

    requisito 15

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const primeiroBotao = document.querySelector('button');
 primeiroBotao.addEventListener('click', () => {
   const inputEmail = document.querySelector('input');
   const inputSenha = document.querySelectorAll('input')[1];
-  
-  if (inputEmail.value === 'tryber@teste.com' || inputSenha == '123456') {
+  if (inputEmail.value === 'tryber@teste.com' || inputSenha === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-})
+});
