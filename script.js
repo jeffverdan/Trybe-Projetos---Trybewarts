b5be2eabf10e6ad3ad95224826e7e4ec952bba09
aribdavid <aryehbdavid@gmail.com>
2021-11-09 17:58:01 -0300

    Requisitos 15, 16, 17, 18, 19 e adequado ao lint

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const loginButton = document.getElementById('login-button');
 const inputEmail = document.getElementById('input-login');
 const inputSenha = document.getElementById('input-senha');
+const submitButton = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
 
-loginButton.addEventListener('click', ()=>{
-    if(inputEmail.value.length > 0 && inputSenha.value.length >0){
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+loginButton.addEventListener('click', () => {
+  if (inputEmail.value.length > 0 && inputSenha.value.length > 0) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+submitButton.disabled = true;
+agreement.addEventListener('click', () => {
+  if (agreement.checked) {
+    submitButton.disabled = false;
+  } else if (!agreement.checked) {
+    submitButton.disabled = true;
+  }
 });
