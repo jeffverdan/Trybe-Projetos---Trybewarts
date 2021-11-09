9ee2f4930dd78ce390f095624e3d3fd3a6173f51
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2021-11-09 10:30:50 -0300

    requisito3

---
+++ script.js
@@ -0,0 +1,15 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const buttonLoginEnter = document.querySelector("#button-login-enter");
+
+function verify(valueA, valueB) {
+  valueA && valueB ? alert("Olá, Tryber!") : alert("Email ou senha inválidos.");
+}
+
+buttonLoginEnter.addEventListener("click", () => {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const checkEmail = emailValue === "tryber@teste.com";
+  const checkPassword = passwordValue === "123456";
+  verify(checkEmail, checkPassword);
+});
