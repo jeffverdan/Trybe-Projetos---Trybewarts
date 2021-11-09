fa9e6c3141d9bb7f229545e68b5d2f950c25de13
Henrique Serres <henrique@gmail.com>
2021-11-09 17:02:15 -0300

    exercicios 1 ao 6

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnEntra = document.getElementById('btn-entra');
+const inputMail = document.getElementById('email');
+const inputSenha = document.getElementById('senha');
+
+btnEntra.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (inputMail.value === 'tryber@teste.com' && inputSenha.value === '123456'){
+    alert('Olá, Tryber!')
+  } else {
+    alert('Email ou senha inválidos.')
+  }
+
+})
