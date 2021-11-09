3a3e856873591172c93a362b1cc1075ff2e65f73
Pedro Candido <pedrohcn02@gmail.com>
2021-11-09 19:49:01 -0300

    tentando

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const submitBtn = document.querySelector('#submit-btn');
 const checkboxAgree = document.querySelector('#agreement');
 const textArea = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
+const mainForm = document.querySelector('#evaluation-form');
+const main = document.querySelector('#main-container');
+const inputName = document.querySelector('#input-name');
+const inputLastname = document.querySelector('#input-lastname');
+const secondEmail = document.querySelector('#input-email');
+const houseSelector = document.querySelector('#house');
 
 btn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -27,4 +33,13 @@ checkboxAgree.addEventListener('click', () => {
 
 textArea.addEventListener('keyup', () => {
   counter.innerHTML = 500 - textArea.value.length;
+});
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  mainForm.style.display = 'none';
+  let para = document.createElement('p')
+  para.className = 'new'
+  para.innerHTML = `Name: ${inputName.value} ${inputLastname.value} <br> Email: ${secondEmail.value} <br> Casa: ${houseSelector.value} <br> Família: ${ratioValue}`;
+  main.appendChild(para)
 });
