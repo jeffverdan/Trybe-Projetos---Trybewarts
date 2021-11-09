fd1e9fd4f78871af73ae13f7a64240582013008f
Andreyrvs <cortaesai@gmail.com>
2021-11-09 15:52:42 -0300

    Requisitos 14 a 18

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const headerInputEmail = document.querySelector('.header-input-email');
 const headerInputSenha = document.querySelector('.headerInputSenha');
 const headerBtnEntrar = document.querySelector('.header-btn-entrar');
-
-
+const inputAgreement = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
 
 headerBtnEntrar.addEventListener('click', () => {
   if (
@@ -13,3 +13,13 @@ headerBtnEntrar.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+function confirmarEnvio() {
+  if (inputAgreement.checked === true) {
+    btnSubmit.disabled = false;
+  } else if (inputAgreement.checked === false) {
+    btnSubmit.disabled = true;
+  }
+}
+
+inputAgreement.addEventListener('change', confirmarEnvio);
