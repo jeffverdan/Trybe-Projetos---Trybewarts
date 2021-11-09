e6dbcb135b88403d6c2a0115415877b81b176d2a
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2021-11-09 16:28:08 -0300

    Adiciona requisito 4 e correçoes.

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const email = document.getElementById('input-email');
 const password = document.getElementById('input-pass');
 
 button.addEventListener('click', () => {
-  if (email.value === '') {
-    alert('Preencha todos os campos em branco');
-  } else if (email.value === 'tryber@teste.com' && password.value === '123456') {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
