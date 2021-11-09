bef9407fa650dc8392c4fb72ad8bd34e2c9bf724
pedro <hmfpedro@hotmail.com>
2021-11-09 15:42:46 -0300

    add exer 3 e 4

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginBtn = document.getElementById('loginBtn');
+const inputEmail = document.getElementById('inputEmail');
+const inputPassword = document.getElementById('inputPassword');
+
+
+  loginBtn.addEventListener('click', () => {
+    if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+      alert('Olá, Tryber!')
+    } else {
+      alert('Email ou senha inválidos.')
+    }
+  }) 
+
