57f3069761650bc83d892b832d0ec906213070b9
Eric Schineider <eric-schineider@outlook.com>
2021-11-09 15:00:49 -0300

    [Eric Schineider e Danilo do Valle] Projeto Tribwards

---
+++ script.js
@@ -0,0 +1,31 @@
+const btnEnter = document.getElementById('btnEnter');
+const email = document.getElementById('email');
+const password = document.getElementById('password')
+const btnSend = document.getElementById('submit-btn')
+const checkBox = document.getElementById('agreement')
+// Mensagem e efeito de login
+btnEnter.addEventListener('click', function(){
+if(email.value == "tryber@teste.com" && password.value == "123456") {
+    alert('Olá, Tryber!')
+} else {
+alert("Email ou senha inválidos")
+}
+});
+
+// Habilitar botão de enviar
+window.onload = () => {
+    btnSend.disabled = true
+}
+    
+
+checkBox.addEventListener('click', function(){
+       
+    if(checkBox.checked) {
+        btnSend.disabled = false;
+    } else {
+        btnSend.disabled = true;
+    }
+})
+
+
+
