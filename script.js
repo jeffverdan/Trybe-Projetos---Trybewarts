fe9e8503dfb67b57412e068991bdd34474354cf8
Matheus Carvalho <matheusrelvinhas@gmail.com>
2021-11-09 20:13:50 -0300

    requisito 14

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("login").addEventListener("click", function (event) {
+  if (
+    document.getElementById("email").value.includes("@", ".com") &&
+    document.getElementById("password").value.length > 4
+  ) {
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    window.alert("Olá, Tryber!");
+  } else {
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    window.alert("Email ou senha inválidos.");
+  }
+});
