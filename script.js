38877dd3ffcc38ea01001909c8a961b0f22779f9
Erik Duarte Rodrigues <erikduarterodrigues@gmail.com>
2021-11-09 19:33:45 -0300

    Entrega desafios e correcao lint

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const email1 = document.querySelector('#input-email-login');
 const senha = document.querySelector('#input-senha');
 const login1 = document.querySelector('#login-btn');
 
-function checkEmail(){
-    if ( email1.value === 'tryber@teste.com' && senha.value === '123456'){
+function checkEmail() {
+    if (email1.value === 'tryber@teste.com' && senha.value === '123456') {
         alert('Olá, Tryber!')
     }
-    else{
+    else {
         alert('Email ou senha inválidos.');
     }
 }
