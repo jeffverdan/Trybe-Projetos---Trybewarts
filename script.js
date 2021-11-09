e5378d72599bb506377e61df61603760e187e57c
Jefferson [Jeffrey] <dantas.jefferson03@gmail.com>
2021-11-09 16:09:43 -0300

    requisito 3 teste

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btnSubmit = document.getElementById('btn-submit');
 btnSubmit.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log('evento funcionando'); /* só pra testar */
+  const email = document.getElementsByName('email');
+  const senha = document.getElementsByName('password');
+  console.log(email, senha);
 });
