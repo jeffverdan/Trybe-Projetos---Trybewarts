a020d7229e748a70f65090023e28e64ca9b0d37f
Eric Schineider <eric-schineider@outlook.com>
2021-11-09 15:49:44 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const btnEnter = document.getElementById('btnEnter');
 const email = document.getElementById('email');
-const password = document.getElementById('password')
-const btnSend = document.getElementById('submit-btn')
-const checkBox = document.getElementById('agreement')
+const password = document.getElementById('password');
+const btnSend = document.getElementById('submit-btn');
+const checkBox = document.getElementById('agreement');
+let counter = document.getElementById('counter');
+const textarea = document.getElementById('textarea');
 // Mensagem e efeito de login
 btnEnter.addEventListener('click', function(){
 if(email.value == "tryber@teste.com" && password.value == "123456") {
@@ -26,6 +28,17 @@ checkBox.addEventListener('click', function(){
         btnSend.disabled = true;
     }
 })
+
+
+// Contagem de caracteres 
+
+let textarea2 = textarea.getAttribute('maxlength');
+counter.innerHTML = textarea2;
+textarea.addEventListener('keyup', function () {
+   document.getElementById('counter').innerHTML = textarea2 - this.value.length;
+});
+
+counter.innerHTML = textarea2;
 
 
 
