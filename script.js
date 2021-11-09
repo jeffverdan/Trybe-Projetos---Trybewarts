abea52d154bd643b39e57491d67770a08054fb5c
gabriel morais <gabriel.f.m@hotmail.com>
2021-11-09 18:19:39 -0300

    erro lint 4

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const email = document.querySelector("email");
-const password = document.querySelector("password");
+const submit = document.getElementById('submit');
 
-function alerta() {
+submit.addEventListener('click', () => {
   if (email === 'ryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos');
   }
-}
-
-alerta();
+});
