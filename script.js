df696ad7d0babec8587decf94b15eea8934f0d57
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2021-11-09 17:19:11 -0300

    Requisitos 20 e 21

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const primeiroBotao = document.querySelector('button');
+const formPrincipal = document.getElementById('evaluation-form');
+const textArea = document.getElementById('textarea');
+const contadorDeCaracteres = document.getElementById('counter');
 const botaoEnviar = document.getElementById('submit-btn');
 const checkboxAgreement = document.getElementById('agreement');
+const inputNome = document.getElementById('input-name');
+const inputSobrenome = document.getElementById('input-lastname');
+const inputEmailFormPrincipal = document.getElementById('input-email');
+const inputCasa = document.getElementById('house');
 
 primeiroBotao.addEventListener('click', () => {
   const inputEmail = document.querySelector('input');
@@ -18,4 +25,54 @@ checkboxAgreement.addEventListener('change', () => {
   } else {
     botaoEnviar.disabled = true;
   }
+});
+
+textArea.addEventListener('input', () => {
+  const numeroDeCaracteres = textArea.textLength;
+  contadorDeCaracteres.innerText = 500;
+  contadorDeCaracteres.innerText -= numeroDeCaracteres;
+});
+
+function obterMateriasSelecionadas() {
+  const checkboxConteudos = document.querySelectorAll('.subject:checked');
+  let materias = '';
+  for (let i = 0; i < checkboxConteudos.length; i += 1) {
+    if (i === checkboxConteudos.length) {
+      materias += `${checkboxConteudos[i].value}`;
+    }
+    materias += `${checkboxConteudos[i].value} `;
+  }
+
+  return materias;
+}
+
+function mostrarInformacoes(informacoes) {
+  const ul = document.createElement('ul');
+  const chaves = Object.keys(informacoes);
+  const valores = Object.values(informacoes);
+  for (let i = 0; i < chaves.length; i += 1) {
+    const li = document.createElement('li');
+    li.innerText = `${chaves[i]}: ${valores[i]}`;
+    ul.appendChild(li);
+  }
+  formPrincipal.innerHTML = '';
+  formPrincipal.appendChild(ul);
+}
+
+botaoEnviar.addEventListener('click', (event) => {
+  event.preventDefault();
+  const inputAvaliacao = document.querySelector('input[name="rate"]:checked');
+  const selectFamilia = document.querySelector('input[name="family"]:checked');
+  const materias = obterMateriasSelecionadas();
+  const informacoes = {
+    Nome: `${inputNome.value} ${inputSobrenome.value}`,
+    Email: inputEmailFormPrincipal.value,
+    Casa: inputCasa.value,
+    Família: selectFamilia.value,
+    Matérias: materias,
+    Avaliação: inputAvaliacao.value,
+    Observações: textArea.value,
+  };
+
+  mostrarInformacoes(informacoes);
 });
