aeb40358eeef8addeda7affaa8b7e1381d5be770
Igor <igormelojunqueira@gmail.com>
2021-11-09 14:53:27 -0300

    Iniciando projeto requisitos 1 a 11

---
+++ script.js
@@ -0,0 +1,12 @@
+// Requisito 3 
+function login () {
+  const email = document.getElementById('email-input');
+  const password = document.getElementById ('password-input');
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    return alert ('Olá, Tryber!');
+  } else {
+    return alert ('Email ou senha inválidos.');
+  }
+}
+
+document.getElementById('btn-login').addEventListener('click', login);
