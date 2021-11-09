92eb4a2b066fff243962e2a052989b7d148ac1db
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-11-09 18:58:26 -0300

    adiciona arquivos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const entrar = document.querySelector('.entrar');
 const email = document.querySelector('.email');
 const senha = document.querySelector('.senha');
+const checkbox = document.querySelector('#agreement');
+const btn = document.querySelector('#submit-btn');
 
 function login() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -10,3 +12,13 @@ function login() {
   }
 }
 entrar.addEventListener('click', login);
+
+function habilitarBotao() {
+  if (checkbox.checked === true) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+}
+
+checkbox.addEventListener('click', habilitarBotao);
