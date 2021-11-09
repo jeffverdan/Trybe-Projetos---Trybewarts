1082bbbf80d0357aa4881bc9b2a8712f39e078f8
Antonio Sabino <aslimajr@outlook.com>
2021-11-09 18:48:25 -0300

    Resolucao requisito 18

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const btnLogin = document.getElementById('btn-login');
+const btnSubmit = document.getElementById('submit-btn');
+const checkAgreement = document.getElementById('agreement');
+
+btnSubmit.disabled = true;
 
 btnLogin.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+checkAgreement.addEventListener('click', () => {
+  const agreement = checkAgreement.checked;
+  if (agreement) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
   }
 });
