90dcc9f503779bf052d3d3a03ed55f75710ef8c5
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2021-11-09 16:23:39 -0300

    Adiciona requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('btn-entrar');
+const email = document.getElementById('input-email');
+const password = document.getElementById('input-pass');
+
+button.addEventListener('click', () => {
+  if (email.value === '') {
+    alert('Preencha todos os campos em branco');
+  } else if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
