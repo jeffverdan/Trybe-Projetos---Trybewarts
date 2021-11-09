da7339e8054599d4e28ba7be84f5c24c9286ef93
aribdavid <aryehbdavid@gmail.com>
2021-11-09 19:43:06 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const inputEmail = document.getElementById('input-login');
 const inputSenha = document.getElementById('input-senha');
 const submitButton = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
-
+const textarea = document.getElementById('textarea');
 loginButton.addEventListener('click', () => {
   if (inputEmail.value.length > 0 && inputSenha.value.length > 0) {
     alert('Olá, Tryber!');
@@ -11,6 +11,15 @@ loginButton.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+// counter para text area
+textarea.addEventListener('input', () =>{
+	let count = 500 - textarea.value.length;
+	document.getElementById('counter').innerText = count;
+});
+
+
+// HABILITA BOTAO ENVIAR SOMENTE AO SELECIONAR O CHECKBOX
 submitButton.disabled = true;
 agreement.addEventListener('click', () => {
   if (agreement.checked) {
