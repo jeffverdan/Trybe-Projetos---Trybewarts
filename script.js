665b20b7a1a8149a9e3137fb6f969193fcf14d21
gabriel morais <gabriel.f.m@hotmail.com>
2021-11-09 18:28:18 -0300

    erro lint 404.2

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const submit = document.getElementById('submit');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
-
-
 submit.addEventListener('click', () => {
   if (email === 'ryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
