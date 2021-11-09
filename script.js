7435483b8ae8677b67353ae82ca6fa92a9226dee
pedro <hmfpedro@hotmail.com>
2021-11-09 18:18:32 -0300

    lintlintlint

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const loginBtn = document.getElementById("loginBtn");
-const inputEmail = document.getElementById("inputEmail");
-const inputPassword = document.getElementById("inputPassword");
+const loginBtn = document.getElementById('loginBtn');
+const inputEmail = document.getElementById('inputEmail');
+const inputPassword = document.getElementById('inputPassword');
 
-loginBtn.addEventListener("click", () => {
-  if (
-    inputEmail.value === "tryber@teste.com" &&
-    inputPassword.value === "123456"
-  ) {
-    alert("Olá, Tryber!");
+loginBtn.addEventListener('click', () => {
+  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 });
