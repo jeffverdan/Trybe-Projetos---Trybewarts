68c38e902bafa7028b30b4fe6720b2a7029519f0
Ruy <ruuybarbosajr@icloud.com>
2021-11-09 16:03:24 -0300

    adiciona requisitos obrigatorios

---
+++ script.js
@@ -0,0 +1,22 @@
+const btnEnter = document.querySelector("#btn-enter");
+const inputEmail = document.querySelector("#login-email");
+const inputPassword = document.querySelector("#input-password");
+const btnSubmit = document.querySelector("#submit-btn");
+const inputSubmit = document.querySelector("#agreement");
+
+btnEnter.addEventListener("click", () => {
+  if (
+    inputEmail.value === "tryber@teste.com" &&
+    inputPassword.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+inputSubmit.addEventListener('click',()=>{
+  if(inputSubmit.checked == true){
+    btnSubmit.removeAttribute('disabled')
+  }
+})
