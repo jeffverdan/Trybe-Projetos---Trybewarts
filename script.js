c1995a9ab92b45e0878d4b326569b7c5e61647ca
gabriel morais <gabriel.f.m@hotmail.com>
2021-11-09 18:05:12 -0300

    erro lint

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const email = document.querySelector("email");
 const password = document.querySelector("password");
 
 function alerta() {
-  if (email == "tryber@teste.com" && password == "123456") {
-    alert("Olá, Tryber!");
+  if (email === 'ryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos");
+    alert('Email ou senha inválidos');
   }
 }
