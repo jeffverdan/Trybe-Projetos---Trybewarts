e0db193967302399679650e29c8f32562cac2a84
Ruy <ruuybarbosajr@icloud.com>
2021-11-09 16:13:29 -0300

    correçao

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const btnEnter = document.querySelector("#btn-enter");
-const inputEmail = document.querySelector("#login-email");
-const inputPassword = document.querySelector("#input-password");
-const btnSubmit = document.querySelector("#submit-btn");
-const inputSubmit = document.querySelector("#agreement");
+const btnEnter = document.querySelector('#btn-enter');
+const inputEmail = document.querySelector('#login-email');
+const inputPassword = document.querySelector('#input-password');
+const btnSubmit = document.querySelector('#submit-btn');
+const inputSubmit = document.querySelector('#agreement');
 
-btnEnter.addEventListener("click", () => {
+btnEnter.addEventListener('click', () => {
   if (
-    inputEmail.value === "tryber@teste.com" &&
-    inputPassword.value === "123456"
+    inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456'
   ) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 });
 
-inputSubmit.addEventListener('click',()=>{
-  if(inputSubmit.checked == true){
-    btnSubmit.removeAttribute('disabled')
+inputSubmit.addEventListener('click', () => {
+  if (inputSubmit.checked) {
+    btnSubmit.removeAttribute('disabled');
   }
-})
+});
