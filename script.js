80229fe499c027ed9e3ea297e48de48da04045e3
SamuelDAlencar <sarl.2001@hotmail.com>
2021-11-09 16:24:25 -0300

    conclusão requisitos 9, 8, 10, e 11

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-window.onload = function () {
-  
-};
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const btnEntrar = document.getElementById('btn-entrar');
+
+btnEntrar.addEventListener('click', (event) => {
+  if (email.value !== 'tryber@teste.com' && senha.value !== '123456') {
+    event.preventDefault();
+    alert('Email ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+});
