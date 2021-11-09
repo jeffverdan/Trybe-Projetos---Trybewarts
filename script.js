824b44620f7a141d4c117a07af552e08d61b90a3
Renato <renatoft89@gmail.com>
2021-11-09 18:22:37 -0300

    requisito-18

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ btnLogin.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+document.getElementById("submit-btn").disabled = true;
+// seleciona o button e desabilita.
+const checkInfo = document.getElementById("agreement");
+// verifica o conteudo do check
+checkInfo.addEventListener("input", () => {
+  if ( checkInfo !== null ) {
+    //habilita o botão
+    document.getElementById("submit-btn").disabled = false;
+  } else {
+    //desabilita o botão se o conteúdo do input ficar em branco
+    document.getElementById("submit-btn").disabled = true;
+  }
+});
