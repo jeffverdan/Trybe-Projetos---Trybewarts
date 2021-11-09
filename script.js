5dc23f5ed66850f8778b797378d474b511c81a4e
Felipe Barboza <felipebarboza5@gmail.com>
2021-11-09 16:09:01 -0300

    Projeto finalizado

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector('#btn-entrar').addEventListener('click', () => {
+  const email = document.querySelector('#email-login').value;
+  const senha = document.querySelector('#senha-login').value;
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+checkbox = document.querySelector('#agreement');
+
+
+
+checkbox.addEventListener('click', () => {
+    if (checkbox.checked === true){
+      document.querySelector('#submit-btn').disabled = false
+    }  
+
+    if (checkbox.checked === false){
+      document.querySelector('#submit-btn').disabled = true
+    }  
+})
