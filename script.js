4b13093062e21fb548d59f4d054789f8e5bc49ea
Guilherme <guilhermehallmann@gmail.com>
2021-11-09 16:24:05 -0300

    Resolução requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnEntrar = document.querySelector("#loginBtn");
+const mailInput = document.querySelector("#email");
+const senhaInput = document.querySelector("#senha");
+
+btnEntrar.addEventListener("click", function () {
+  if(mailInput.value === 'tryber@teste.com' && senhaInput.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
