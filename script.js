bf63abd82fdadc7355bf6f187824ed8ece0f9ea8
Ingrid Louise <ingridlouise7@gmail.com>
2021-11-09 21:19:36 -0300

    feito quesito css e js dos quesitos 1 ao 4

---
+++ script.js
@@ -0,0 +1,10 @@
+const botao = document.querySelector('.trybewarts-login');
+const email = 'tryber@teste.com';
+const password = '123456';
+function exibeAlert() {
+if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!')
+} else {
+    alert('Email ou senha inválidos.')
+}
+};
