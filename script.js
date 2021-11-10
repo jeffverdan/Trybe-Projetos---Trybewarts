9be13740d8892e8fa1a9ed26636782f46d341df9
Jeferson Verdan <jeff_verdan@outlook.com>
2021-11-10 14:02:12 -0300

    Quase lá

---
+++ script.js
@@ -0,0 +1,34 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const loginBtn = document.getElementById('loginBtn');
+
+
+//Função para validar login
+function valLogin() {
+    const e = email.value;
+    const p = password.value;
+    if (e === 'tryber@teste.com' && p === '123456') {
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.');
+    }
+}
+loginBtn.addEventListener('click',valLogin);
+
+// Função que trava e destrava Botão de Eviar de acordo com checkbox
+const send = document.getElementById('submit-btn');
+send.disabled = true;  //referencia https://www.w3schools.com/jsref/prop_pushbutton_disabled.asp
+const agreement = document.getElementById('agreement');
+
+function destravaBtn(e) {
+    if (e.target.checked === true) {
+        send.disabled = false;
+    } else {
+        send.disabled = true;
+    }
+}
+agreement.addEventListener('click',destravaBtn);
+
+
+
+
