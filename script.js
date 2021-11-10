e54ceedf5e2911f2af1b35f35aca67da7bf53b0e
EuSouLuis <luisaraujodemelo@hotmail.com>
2021-11-09 21:42:57 -0300

    lint css

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const btnLogin = document.getElementById("btnLogin");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
-const btnEnviar = document.getElementById("submit-btn");
-const checkBox = document.getElementById("agreement");
+const btnLogin = document.getElementById('btnLogin');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const btnEnviar = document.getElementById('submit-btn');
+const checkBox = document.getElementById('agreement');
 
 window.onload = function desabilita() {
   checkBox.disbaled = true;
 };
 
 function login() {
-  if (email.value === "tryber@teste.com" && senha.value === "123456") {
-    alert("Olá, Tryber!");
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
 
 function ativaBtnEnviar() {
-  if (checkBox.disbaled == false) {
-    btnEnviar.disabled == false;
+  if (checkBox.disbaled === false) {
+    btnEnviar.disabled === false;
   } else {
-    btnEnviar.disabled == true;
-    btnEnviar.style.backgroundColor = "purple";
+    btnEnviar.disabled === true;
+    btnEnviar.style.backgroundColor = 'purple';
   }
 }
-btnLogin.addEventListener("click", login);
-checkBox.addEventListener("click", ativaBtnEnviar);
+btnLogin.addEventListener('click', login);
+checkBox.addEventListener('click', ativaBtnEnviar);
 console.log(checkBox.checked);
