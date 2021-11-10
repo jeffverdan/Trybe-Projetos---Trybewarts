f336a30c1542f40524d1dd9d6aac388c93a0e0c7
Pedro Candido <pedrohcn02@gmail.com>
2021-11-09 21:47:31 -0300

    finalizado

--- script.js
+++ script.js
@@ -6,11 +6,30 @@ const checkboxAgree = document.querySelector('#agreement');
 const textArea = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
 const mainForm = document.querySelector('#evaluation-form');
-const main = document.querySelector('#main-container');
 const inputName = document.querySelector('#input-name');
 const inputLastname = document.querySelector('#input-lastname');
 const secondEmail = document.querySelector('#input-email');
 const houseSelector = document.querySelector('#house');
+let ratioValue = document.querySelector('input[name="family"]:checked').value;
+const checkedBoxes = document.querySelectorAll('.subject');
+let rating = document.querySelector('input[name="rate"]:checked').value;
+const ratingDiv = document.querySelector('#rating-container');
+const family = document.querySelector('.labels');
+const boxes = [];
+
+function checkBoxes() {
+  for (let i = 0; i < checkedBoxes.length; i += 1) {
+    if (checkedBoxes[i].checked === true) {
+      boxes.push(` ${checkedBoxes[i].value}`);
+    }
+  }
+}
+
+family.addEventListener('click', (event) => {
+  if (event.target.checked === true) {
+    ratioValue = document.querySelector('input[name="family"]:checked').value;
+  }
+});
 
 btn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -35,11 +54,26 @@ textArea.addEventListener('keyup', () => {
   counter.innerHTML = 500 - textArea.value.length;
 });
 
+ratingDiv.addEventListener('click', (event) => {
+  if (event.target.checked === true) {
+    rating = document.querySelector('input[name="rate"]:checked').value;
+  }
+});
+
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  mainForm.style.display = 'none';
-  let para = document.createElement('p')
-  para.className = 'new'
-  para.innerHTML = `Name: ${inputName.value} ${inputLastname.value} <br> Email: ${secondEmail.value} <br> Casa: ${houseSelector.value} <br> Família: ${ratioValue}`;
-  main.appendChild(para)
+  checkBoxes();
+
+  const para = document.createElement('p');
+  para.className = 'new';
+  para.innerHTML = `Nome: ${inputName.value} 
+  ${inputLastname.value} <br> Email: 
+  ${secondEmail.value} <br> Casa: 
+  ${houseSelector.value} <br> Família: 
+  ${ratioValue} <br> Matérias: 
+  ${boxes}<br> Avaliação: 
+  ${rating} <br> Observações: 
+  ${textArea.value}`;
+  mainForm.innerHTML = '';
+  mainForm.appendChild(para);
 });
